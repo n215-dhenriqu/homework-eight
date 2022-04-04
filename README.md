de8d8b1021cb257a50a0eabeb6b1147158f9a153
denisse henriquez <denissehenriquez@hotmail.com>
2022-04-04 14:14:12 -0400

    hw8

---
+++ README.md
@@ -0,0 +1,14 @@
+# HOMEWORK 8
+
+## I used jQuery
+
+For this homework assignment, you will:
+
+You must create a project named homework-eight.
+Create a navigation bar that has four links
+Add content to the page when you click on a link by using jQuery
+Use only one page and load all the content onto that page
+All pages must have full content. Images and text on every page and professionally styled.
+Upload your project to Github and Web4 and add your Web4 link to the README file.
+
+https://in-info-web4.informatics.iupui.edu/~dhenriqu/homework-eight/#
---
+++ app/app.js
@@ -0,0 +1,241 @@
+var homeContent = `<div class="hero"></div>
+        
+
+<div class="content">
+    <h1>HOME</h1>
+    <div class="products">
+      <div class="product">
+        <div class="prod-image"></div>
+        <div class="prod-desc">
+          <h4>Image</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            blanditiis libero similique labore nostrum, facere dolore
+            aperiam quibusdam unde maxime magnam beatae iusto, qui vero cum
+            accusantium ipsum facilis debitis. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Illum soluta quam quo, similique
+            sed possimus necessitatibus veniam porro commodi facere! Quod
+          </p>
+        </div>
+        
+    </div>
+    
+
+    
+  </div>
+
+  <div class="products">
+    <div class="product">
+      <div class="prod-images"></div>
+      <div class="prod-desc">
+        <h4>Image</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          blanditiis libero similique labore nostrum, facere dolore
+          aperiam quibusdam unde maxime magnam beatae iusto, qui vero cum
+          accusantium ipsum facilis debitis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Illum soluta quam quo, similique
+          sed possimus necessitatibus veniam porro commodi facere! Quod
+        </p>
+      </div>
+      
+  </div>
+  
+
+  
+</div>
+
+<div class="products">
+    <div class="product">
+      <div class="prods-image"></div>
+      <div class="prod-desc">
+        <h4>Image</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          blanditiis libero similique labore nostrum, facere dolore
+          aperiam quibusdam unde maxime magnam beatae iusto, qui vero cum
+          accusantium ipsum facilis debitis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Illum soluta quam quo, similique
+          sed possimus necessitatibus veniam porro commodi facere! Quod
+        </p>
+      </div>
+      
+  </div>
+  
+
+  
+</div>
+
+<div class="products">
+    <div class="product">
+      <div class="prods-images"></div>
+      <div class="prod-desc">
+        <h4>Image</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          blanditiis libero similique labore nostrum, facere dolore
+          aperiam quibusdam unde maxime magnam beatae iusto, qui vero cum
+          accusantium ipsum facilis debitis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Illum soluta quam quo, similique
+          sed possimus necessitatibus veniam porro commodi facere! Quod
+        </p>
+      </div>
+      
+  </div>
+  
+
+  
+</div>
+
+<div class="products">
+    <div class="product">
+      <div class="prod-imaged"></div>
+      <div class="prod-desc">
+        <h4>Image</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          blanditiis libero similique labore nostrum, facere dolore
+          aperiam quibusdam unde maxime magnam beatae iusto, qui vero cum
+          accusantium ipsum facilis debitis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Illum soluta quam quo, similique
+          sed possimus necessitatibus veniam porro commodi facere! Quod
+        </p>
+      </div>
+      
+  </div>
+  
+
+  
+</div>
+
+</div>
+</div>`;
+
+var aboutContent = ` <div class="banner"></div>
+<div class="fullpage">
+    <h1>ABOUT</h1>
+    <div class="pic">
+        <div class="trip1"></div>
+        <div class="trip2"></div>
+    </div>
+
+    <div class="home-page">
+        <div class="text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum convallis, dolor lorem porta magna, eu consequat lorem magna a turpis. Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus molestie lacinia dui in tincidunt. Morbi vulputate accumsan scelerisque. Nunc justo turpis, blandit ac vestibulum congue, ullamcorper mattis orci. Mauris in libero id lectus pellentesque sollicitudin. Vivamus convallis, erat viverra congue porttitor, justo enim hendrerit eros, eget consequat velit sapien ac ligula. Duis ac porta nulla, non mattis ligula.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum convallis, dolor lorem porta magna, eu consequat lorem magna a turpis. Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus molestie lacinia dui in tincidunt. Morbi vulputate accumsan scelerisque. Nunc justo turpis, blandit ac vestibulum congue, ullamcorper mattis orci. Mauris in libero id lectus pellentesque sollicitudin. Vivamus convallis, erat viverra congue porttitor, justo enim hendrerit eros, eget consequat velit sapien ac ligula. Duis ac porta nulla, non mattis ligula.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac bibendum quam, vel finibus leo. Morbi mattis, odio sed fermentum convallis, dolor lorem porta magna, eu consequat lorem magna a turpis. Vivamus ultrices elit risus, at egestas enim tempus non. Vivamus molestie lacinia dui in tincidunt. Morbi vulputate accumsan scelerisque. Nunc justo turpis, blandit ac vestibulum congue, ullamcorper mattis orci. Mauris in libero id lectus pellentesque sollicitudin. Vivamus convallis, erat viverra congue porttitor, justo enim hendrerit eros, eget consequat velit sapien ac ligula. Duis ac porta nulla, non mattis ligula.</p>
+        </div>
+        <div class="text2">
+            <p>Curabitur tellus erat, commodo at dapibus ac, tempus quis justo. Suspendisse tempor arcu risus, at euismod nisi ullamcorper id. Sed eros quam, elementum at consequat sit amet, fringilla sed metus. Sed laoreet fermentum augue. Quisque viverra magna vitae vehicula sodales. Mauris scelerisque pellentesque maximus. Praesent sed augue luctus, sollicitudin tortor at, aliquam ex. Nunc ac eros tristique, lacinia urna ac, eleifend massa.</p>
+            <p>Curabitur tellus erat, commodo at dapibus ac, tempus quis justo. Suspendisse tempor arcu risus, at euismod nisi ullamcorper id. Sed eros quam, elementum at consequat sit amet, fringilla sed metus. Sed laoreet fermentum augue. Quisque viverra magna vitae vehicula sodales. Mauris scelerisque pellentesque maximus. Praesent sed augue luctus, sollicitudin tortor at, aliquam ex. Nunc ac eros tristique, lacinia urna ac, eleifend massa.</p>
+            <p>Curabitur tellus erat, commodo at dapibus ac, tempus quis justo. Suspendisse tempor arcu risus, at euismod nisi ullamcorper id. Sed eros quam, elementum at consequat sit amet, fringilla sed metus. Sed laoreet fermentum augue. Quisque viverra magna vitae vehicula sodales. Mauris scelerisque pellentesque maximus. Praesent sed augue luctus, sollicitudin tortor at, aliquam ex. Nunc ac eros tristique, lacinia urna ac, eleifend massa.</p>
+        </div>
+
+        
+    </div>`
+
+var productsContent = `<div class="banners"></div>
+<div class="fullpages">
+    <h1>PRODUCTS</h1>
+    <div class="item">
+        <div class="items">
+            <div class="item-pic"></div>
+            <div class="item-text">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio doloremque cupiditate maiores, accusantium totam qui aliquam temporibus quasi voluptatum minima quas quo. Numquam quae necessitatibus, nihil consequatur veritatis dignissimos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur veniam obcaecati animi fugit possimus vel dicta illo deserunt? Odit sint qui possimus eveniet assumenda enim modi corrupti iure autem?</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="items">
+            <div class="item-pics"></div>
+            <div class="item-text">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio doloremque cupiditate maiores, accusantium totam qui aliquam temporibus quasi voluptatum minima quas quo. Numquam quae necessitatibus, nihil consequatur veritatis dignissimos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis tempora deserunt error, sit repellat hic eos a! Itaque blanditiis ipsam nulla est dolorem voluptates voluptatem autem ipsum hic pariatur.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="items">
+            <div class="items-pic"></div>
+            <div class="item-text">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae, distinctio doloremque cupiditate maiores, accusantium totam qui aliquam temporibus quasi voluptatum minima quas quo. Numquam quae necessitatibus, nihil consequatur veritatis dignissimos.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure consequatur asperiores dolores quos nobis, aperiam, voluptatum adipisci odio provident natus optio aliquid veniam. Veniam reiciendis fugit ad, aliquam iusto consectetur!</p>
+            </div>
+        </div>
+    </div>
+
+    
+
+   
+     
+</div>`
+
+var contactContent = `<div class="logo"></div>
+<div class="entire-page">
+    <h1>CONTACT</h1>
+</div>
+<div class="outer">
+
+    <div class="form">
+        <p>First Name:</p>
+        <div class="box"></div>
+    </div>
+    <div class="form">
+        <p>Last Name:</p>
+        <div class="box"></div>
+    </div>
+    <div class="form">
+        <p>Email:</p>
+        <div class="box"></div>
+    </div>
+    <div class="form">
+        <p>Phone Number:</p>
+        <div class="box"></div>
+    </div>
+    
+    
+    <div class="button1">
+        <div class="but1">
+            
+            <p>
+                SUBMIT
+            </p>
+        </div>
+    </div>
+    
+
+   
+     
+</div>`
+
+
+function initlisteners (){
+    $("nav a").click(function (e){
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+        
+        
+        console.log("click " + btnID);
+    });
+}
+
+function loadPageContent(pageName){
+    let contentID = pageName + "Content";
+    $("#app").html(eval(contentID));
+}
+
+
+
+function initApp (){
+    initlisteners();
+}
+
+$(document).ready(function(){
+    loadPageContent("home");
+    initApp();
+})
